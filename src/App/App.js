002527a3a2d9f9f01c5321304c8758e105f7f318
Heba ElGalad <heba957@gmail.com>
2019-03-26 01:00:10 +0200

    Add infinite scroll functionality

--- src/App/App.js
+++ src/App/App.js
@@ -10,82 +10,121 @@ import { CategoriesProvider } from "../contexts/CategoriesContext";
 import { SubCategoryProvider } from "../contexts/SubCategoryContext";
 import { NewsProvider } from "../contexts/NewsContext";
 import { SelectSubCategoryProvider } from "../contexts/SelectSubCategoryContext";
-
-// import api from "../utils/api";
-
 class App extends Component {
   state = {
     categories: [],
     subCategory: [],
     news: [],
     country: "eg",
-    selectSubCategory: ""
+    selectSubCategory: "",
+    page: 1,
+    hasMore: true,
+    isLoading: true
   };
 
-  handelClick = (e) => {
-    e.preventDefault();
-    console.log($(e.target).text());
-    this.setState({
-      selectSubCategory: $(e.target).text()
-    });
-    console.log(this.state);
-    axios
-      .get("https://newsapi.org/v2/top-headlines", {
-        params: {
-          country: this.state.country,
-          category: this.state.selectSubCategory,
-          page: 1,
-          apiKey: "8e873620e5d8423aae1ed3d8d839e3e6"
-        }
-      })
-      .then((response) => {
-        console.log(response.data);
-        this.setState({
-          news: response.data.articles
-        });
-      })
-      .catch((error) => console.log(error));
-  };
-
-  componentDidMount() {
-    const { country, selectSubCategory } = this.state;
+  loadCategories = () => {
     axios
       .get("https://api.myjson.com/bins/qk7qi")
       .then((response) => {
-        console.log(response.data);
+        // console.log(response.data);
         this.setState({
           categories: response.data
         });
       })
       .catch((error) => console.log(error));
+  };
 
+  loadSubCategories = () => {
     axios
       .get("https://api.myjson.com/bins/6frhm?cat=1")
       .then((response) => {
-        console.log(response.data);
+        // console.log(response.data);
         this.setState({
           subCategory: response.data
         });
       })
       .catch((error) => console.log(error));
+  };
 
+  loadArticles = () => {
+    const { country, selectSubCategory, page, news } = this.state;
     axios
       .get("https://newsapi.org/v2/top-headlines", {
         params: {
           country: country,
           category: selectSubCategory ? selectSubCategory : "sports",
-          page: 1,
+          page: page,
           apiKey: "8e873620e5d8423aae1ed3d8d839e3e6"
         }
       })
       .then((response) => {
-        console.log(response.data);
+        // console.log("loadArticles:", response.data);
         this.setState({
-          news: response.data.articles
+          isLoading: false,
+          hasMore: news.length < response.data.totalResults,
+          news: [
+            ...news,
+            ...response.data.articles.map((article) => ({
+              title: article.title,
+              name: article.source.name,
+              url: article.url,
+              time: article.publishedAt,
+              image: article.urlToImage
+            }))
+          ]
         });
-      })
-      .catch((error) => console.log(error));
+      });
+    // .catch((error) => console.log(error));
+  };
+
+  loadMore = () => {
+    this.setState(
+      (prevState) => ({
+        page: prevState.page + 1,
+        isLoading: true
+      }),
+      () => this.loadArticles()
+    );
+  };
+
+  handelScroll = (e) => {
+    const { isLoading, hasMore } = this.state;
+    if (isLoading || !hasMore) return;
+
+    const pageOffset = window.innerHeight + document.documentElement.scrollTop;
+    const lastElementOffset = document.documentElement.offsetHeight;
+    // To start scroll before the last 50px from the bottom
+    const bottomOffset = 50;
+
+    if (pageOffset > lastElementOffset - bottomOffset) {
+      this.loadMore();
+    }
+  };
+
+  handelClick = (e) => {
+    this.setState( ({
+        selectSubCategory: $(e.target).text(),
+        isLoading: true,
+        news: []
+      }),
+      () => this.loadArticles()
+    );
+  };
+
+  componentDidMount() {
+    this.loadCategories();
+
+    this.loadSubCategories();
+
+    this.loadArticles();
+
+    this.scrollListener = window.addEventListener("scroll", (e) => {
+      this.handelScroll(e);
+    });
   }
+
+  // componentWillMount() {
+  // }
 
   render() {
     const { categories, subCategory, news } = this.state;
